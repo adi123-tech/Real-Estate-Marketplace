727cb0093760e50085b4de5c861b5b896ff8434e
Addyyy <agdhonde@gmail.com>
2024-08-09 16:00:57 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const CORS = require("cors");
+app.use(express.json());
+app.use(CORS());
+
+app.get("/hello", (req, res) => {
+  res.send({ msg: "Hello" });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000....................");
+});
