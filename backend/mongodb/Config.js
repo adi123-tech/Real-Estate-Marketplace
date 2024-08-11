300f38a952426d8bf22fcd4ce22bbcb80c8feac9
Addyyy <agdhonde@gmail.com>
2024-08-11 13:08:53 +0530

    Update

--- backend/mongodb/Config.js
+++ backend/mongodb/Config.js
@@ -1,4 +1,11 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URL);
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
